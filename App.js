5a8be972f516f446f66ba79958ba0cd956028dc9
ledat <ledat4657@gmail.com>
2024-04-28 11:35:16 +0700

    update

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import HomeScreen from './src/HomeScrenn';
+import DetailsScreen from './src/DetailsScrenn';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { PaperProvider } from 'react-native-paper';
+import CustomNavigationBar from './src/CustomNavigationBar';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            header: (props) => <CustomNavigationBar {...props} />,
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+};
+
+export default App;
